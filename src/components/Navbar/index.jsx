a3fbd8cb1f1d76d10e54edeb93881b6c2143b4cd
nabanit <nabanitkoirala@gmail.com>
2023-01-17 15:37:23 +0545

    update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './style.module.scss';
 import notification from '../../assets/icons/notification.svg';
 import userImage from '../../assets/icons/user.svg';
 import dropDown from '../../assets/icons/dropdown.svg';
 import mainLogo from '../../assets/icons/mainLogo.png';
 import { Link } from 'react-router-dom';
+import PopupDiv from '../PopupDiv';
+import notification1 from '../../assets/icons/notification1.png';
+import notification2 from '../../assets/icons/notification2.png';
+import clock from '../../assets/icons/clock.png';
+
+
+
+
+
 const Navbar = () => {
     const [routeId, setRouteId] = useState(1)
+    const [showInfo1, setShowInfo1] = useState(false);
+    const [showLogin, setShowLogin] = useState(false)
+
     return (
         <div className={styles.mainContainer} >
             <div className={styles.subMainContainer} >
@@ -37,16 +49,156 @@ const Navbar = () => {
                     </div>
                     <div className={styles.subContainerSecondary} >
 
-                        <div className={styles.notification} >
-                            <img src={notification} alt="" height={11.25} width={10.69} />
+                        <div className={styles.notification}
+                            onClick={() => {
+                                setShowInfo1(true);
+                            }}
+                            role="presentation"
+                        >
+                            <img className={styles.mainImage} src={notification} alt="" height={11.25} width={10.69} />
                             <div className={styles.notificationCount} >6</div>
+
+
+                            <PopupDiv
+                                show={showInfo1}
+                                setShowInfo={setShowInfo1}
+
+                            >
+                                <div className={styles.notificationContainer} >
+                                    <div className={styles.header} >
+                                        <span className={styles.notificationText} >Notification</span>
+                                        <span className={styles.markRead} >Mark all as read</span>
+                                    </div>
+
+                                    <div className={styles.today} >Today</div>
+                                    <div className={styles.notificationDesc} >
+
+                                        <img src={notification1} alt="" width={32} height={32} />
+                                        <div className={styles.details} >
+                                            <span>
+                                                Your issue with order #230456 has been resolved
+                                            </span>
+                                            <div className={styles.timeDetails} >
+                                                <img src={clock} alt="" height={10} width={10} />
+                                                <span>9:15 am</span>
+                                                <span>Tue</span>
+                                                <span>1/10/2023</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div className={styles.notificationDesc} >
+                                        <img src={notification2} alt="" width={32} height={32} />
+                                        <div className={styles.details} >
+                                            <span>
+                                                Withdrawal request verified
+                                            </span><br />
+                                            <span className={styles.contentData} >
+                                                Your issue with order #230456 has been resolved
+                                            </span>
+                                            <div className={styles.timeDetails} >
+                                                <img src={clock} alt="" height={10} width={10} />
+                                                <span>9:15 am</span>
+                                                <span>Tue</span>
+                                                <span>1/10/2023</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.today} >Earlier</div>
+                                    <div className={styles.notificationDesc} >
+                                        <img src={notification1} alt="" width={32} height={32} />
+                                        <div className={styles.details} >
+                                            <span>
+                                                Your issue with order #230456 has been resolved
+                                            </span>
+                                            <div className={styles.timeDetails} >
+                                                <img src={clock} alt="" height={10} width={10} />
+                                                <span>9:15 am</span>
+                                                <span>Tue</span>
+                                                <span>1/10/2023</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.notificationDesc} >
+                                        <img src={notification2} alt="" width={32} height={32} />
+                                        <div className={styles.details} >
+                                            <span>
+                                                Order no #567850 not delivered
+                                            </span>
+                                            <div className={styles.timeDetails} >
+                                                <img src={clock} alt="" height={10} width={10} />
+                                                <span>9:15 am</span>
+                                                <span>Tue</span>
+                                                <span>1/10/2023</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.notificationDesc} >
+                                        <img src={notification2} alt="" width={32} height={32} />
+                                        <div className={styles.details} >
+                                            <span>
+                                                Order no #567851 not delivered
+                                            </span>
+                                            <div className={styles.timeDetails} >
+                                                <img src={clock} alt="" height={10} width={10} />
+                                                <span>9:15 am</span>
+                                                <span>Tue</span>
+                                                <span>1/10/2023</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.seeAll} >See All</div>
+
+                                </div>
+
+                            </PopupDiv>
                         </div>
 
-                        <div className={styles.userLogin} >
+                        <div className={styles.userLogin}
+                            onClick={() => {
+                                setShowLogin(true);
+                            }}
+                            role="presentation"
+                        >
                             <img src={userImage} alt="" height={32} width={32} />
                             <span>Arun</span>
                             <img src={dropDown} alt="" height={10} width={10} />
+                            <PopupDiv
+                                show={showLogin}
+                                setShowInfo={setShowLogin}
 
+                            >
+                                <div className={styles.notificationContainer} >
+                                    <div className={styles.header} >
+                                        Vendor ID. 3728
+                                    </div>
+                                    <div className={styles.header} >
+                                        Dashboard
+                                    </div>
+                                    <div className={styles.header} >
+                                        Orders
+                                    </div>
+                                    <div className={styles.header} >
+                                        Transactions
+                                    </div>
+                                    <div className={styles.header} >
+                                        Account Settings
+                                    </div>
+                                    <div className={styles.header} >
+                                        Log Out
+                                    </div>
+                                    <div className={styles.header} >
+                                        Website
+                                    </div>
+                                    <div className={styles.header} >
+                                        Support
+                                    </div>
+                                    <div className={styles.header} >
+                                        Contact Us
+                                    </div>
+                                </div>
+
+                            </PopupDiv>
                         </div>
                     </div>
 
---
+++ src/components/PopupDiv/index.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-expressions */
+import React, { useEffect, useRef } from 'react';
+import styles from './style.module.scss';
+
+const InfoBox = ({
+    show,
+    children,
+    setShowInfo
+}) => {
+    const ref = useRef(null);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setShowInfo(false)
+            }
+        };
+        document.addEventListener('click', handleClickOutside, true);
+    }, []);
+
+    if (!show) { return null; }
+
+    return (
+        <div ref={ref} id="12" className={styles.mainContainer} >
+            {children}
+        </div>
+    );
+};
+
+export default InfoBox;
